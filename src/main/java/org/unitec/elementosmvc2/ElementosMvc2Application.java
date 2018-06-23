cdfb16466f6e155fb61d69f52bbc31fafdbcf9da
elderiam <elder_iam@live.com.mx>
2018-06-22 20:38:02 -0500

    22/06/18 Servicios REST
                         *Actualizar
                         *Borrar

--- src/main/java/org/unitec/elementosmvc2/ElementosMvc2Application.java
+++ src/main/java/org/unitec/elementosmvc2/ElementosMvc2Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDate;
+import java.time.LocalDate;    //
 
 @SpringBootApplication
 public class ElementosMvc2Application implements CommandLineRunner {
